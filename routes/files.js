7440021f68c95be4ec40b4b8745e2ef6cbdea5a5
Umesh kumar <umeshiit2019@gmail.com>
2020-12-26 12:47:11 +0530

    clear some errors

--- routes/files.js
+++ routes/files.js
@@ -5,49 +5,78 @@ const { send } = require('process');
 const {v4: uuid4} = require('uuid')
 const File = require('../models/file')
 
+// let storage = multer.diskStorage({
+//     destination: (req, file , cb) => cb(null , 'uploads/'),
+//     filename: (req , file , cb) => {
+//         const uniqueName = `${Date.now()}-${Math.round(Math.random()*1E9)}${path.extname(file.originalname)}`;
+//         cb(null , uniqueName);
+//     }
+// })
+
+// let upload = multer({
+//     storage,
+//     limit: {
+//         fileSize: 100000000
+//     }
+// }).single('myfile');
+
+
 let storage = multer.diskStorage({
-    destination: (req, file , cb) => cb(null , 'uploads/'),
-    filename: (req , file , cb) => {
-        const uniqueName = `${Date.now()}-${Math.round(Math.random()*1E9)}${path.extname(file.originalname)}`;
-        cb(null , uniqueName);
-    }
-})
-
-let upload = multer({
-    storage,
-    limit: {
-        fileSize: 100000000
-    }
-}).single('myfile');
-
-router.post('/' , (req, res) => {
-    
-      // Store file
-      upload(req , res , async (err) => {
-          // validate request
-         if(!req.file) {
-             return res.json({error : 'File not attached.'});
-
-          }
-
-          if(err) {
-              return res.status(500).send({error: err.message})
-
-          }
-
-          const file = new File({
-              filename: req.file.filename,
-              uuid: uuid4() ,
-              path: req.file.path ,
-              size: req.file.size
-          });
-
-          const response = await file.save();
-          return res.json({ file:`${process.env.APP_BASE_URL}/files/${response.uuid}`});
-
-      })
-
+    destination: (req, file, cb) => cb(null, 'uploads/') ,
+    filename: (req, file, cb) => {
+        const uniqueName = `${Date.now()}-${Math.round(Math.random() * 1E9)}${path.extname(file.originalname)}`;
+              cb(null, uniqueName)
+    } ,
 });
+
+let upload = multer({ storage, limits:{ fileSize: 1000000 * 100 }, }).single('myfile'); //100mb
+
+
+
+router.post('/', (req, res) => {
+    upload(req, res, async (err) => {
+      if (err) {
+        return res.status(500).send({ error: err.message });
+      }
+        const file = new File({
+            filename: req.file.filename,
+            uuid: uuidv4(),
+            path: req.file.path,
+            size: req.file.size
+        });
+        const response = await file.save();
+        res.json({ file: `${process.env.APP_BASE_URL}files/${response.uuid}` });
+      });
+});
+
+// router.post('/' , (req, res) => {
+    
+//       // Store file
+//       upload(req , res , async (err) => {
+//           // validate request
+//          if(!req.file) {
+//              return res.json({error : 'File not attached.'});
+
+//           }
+
+//           if(err) {
+//               return res.status(500).send({error: err.message})
+
+//           }
+
+//           const file = new File({
+//               filename: req.file.filename,
+//               uuid: uuid4() ,
+//               path: req.file.path ,
+//               size: req.file.size
+//           });
+
+//           const response = await file.save();
+//           return res.json({ file:`${process.env.APP_BASE_URL}files/${response.uuid}`});
+
+//       })
+
+// });
 
 router.post('/send' , async (req , res) => {
     const {uuid , emailTo, emailFrom} = req.body;
@@ -74,7 +103,7 @@ router.post('/send' , async (req , res) => {
         text: `${emailFrom} shared a file with you.` ,
         html: require('../services/emailTemplate')({
             emailFrom: emailFrom,
-            downloadLink: `${process.env.APP_BASE_URL}/files/${file.uuid}`,
+            downloadLink: `${process.env.APP_BASE_URL}files/${file.uuid}`,
             size: parseInt(file.size/1000) + ' KB',
             expires: '24 hours'
         })
